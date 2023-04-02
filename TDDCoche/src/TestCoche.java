ee7ea17de85ea482ffcf871f3e735d2b3389179c
Daniel <danielcollfdz@gmail.com>
2023-04-02 23:29:25 +0200

    Commit 2 TDD V2 Daniel Coll

--- TDDCoche/src/TestCoche.java
+++ TDDCoche/src/TestCoche.java
@@ -6,25 +6,25 @@ import org.junit.jupiter.api.Test;
 class TestCoche {
 
 	@Test
-	void test_al_crear_un_coche_su_velocidad_es_0() {
+	void test_al_crear_un_coche_su_velocidad_es_0DaniColl() {
 		Coche nuevoCoche = new Coche();
 		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
 	@Test
-	void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+	void test_al_acelerar_un_coche_su_velocidad_aumentaDaniColl() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.acelerar(30);
 		Assertions.assertEquals(30, nuevoCoche.velocidad);
 	}
 	@Test
-	void test_al_decelerar_un_coche_su_velocidad_disminuye() {
+	void test_al_decelerar_un_coche_su_velocidad_disminuyeDaniColl() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.acelerar(30);
 		nuevoCoche.decelerar(20);
 		Assertions.assertEquals(10, nuevoCoche.velocidad);
 	}
 	@Test
-	void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_inferior_a_0() {
+	void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_inferior_a_0DaniColl() {
 		Coche nuevoCoche = new Coche();
 		nuevoCoche.acelerar(30);
 		nuevoCoche.decelerar(50);
