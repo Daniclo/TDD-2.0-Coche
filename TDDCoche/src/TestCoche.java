0a2a5056c51a5a16a04ec6e3f7ab8d70d6d84fe7
Daniel <danielcollfdz@gmail.com>
2023-04-02 23:26:34 +0200

    Commit 1 TDD V2 Daniel Coll

---
+++ TDDCoche/src/TestCoche.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TestCoche {
+
+	@Test
+	void test_al_crear_un_coche_su_velocidad_es_0() {
+		Coche nuevoCoche = new Coche();
+		Assertions.assertEquals(0, nuevoCoche.velocidad);
+	}
+	@Test
+	void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+		Coche nuevoCoche = new Coche();
+		nuevoCoche.acelerar(30);
+		Assertions.assertEquals(30, nuevoCoche.velocidad);
+	}
+	@Test
+	void test_al_decelerar_un_coche_su_velocidad_disminuye() {
+		Coche nuevoCoche = new Coche();
+		nuevoCoche.acelerar(30);
+		nuevoCoche.decelerar(20);
+		Assertions.assertEquals(10, nuevoCoche.velocidad);
+	}
+	@Test
+	void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_inferior_a_0() {
+		Coche nuevoCoche = new Coche();
+		nuevoCoche.acelerar(30);
+		nuevoCoche.decelerar(50);
+		Assertions.assertEquals(0, nuevoCoche.velocidad);
+	}
+}
